a493873ca3ffbfc9da3d9334bf6d6e265e68150e
SwagaIvan <swiiivan33@gmail.com>
2024-06-17 16:06:26 +0300

    you
    you

---
+++ SecondClass/JavaClassTwo/src/DClass.java
@@ -0,0 +1,26 @@
+public class DClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It's not an Alien!");
+            System.out.println("And Am scared of Alien");
+        }
+int topScores = 80;
+        if (topScores < 100) {
+            System.out.println("You have got the highest score");
+        }
+
+        int secondTopScore = 60;
+        if ( (topScores > secondTopScore) && (topScores < 100)) {
+            System.out.println("Greater than secondTopScore but less than 100");
+        }
+
+        if ((topScore > 80)|| (secondTopScore <= 80)) {
+            System.out.println("Either or both of the conditions is true");
+
+        }
+    }
+}
---
+++ SecondClass/SecondClass2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main SecondClass2 {
+    public static void main(String[] args) {
+    System.out.println("Helo Ivo,");
+System.out.print("Can we go out tonight");
+}
+}
